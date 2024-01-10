dd0eec1a2e21e31aee131ac233eef3dfa73dc94b
augustobrigo <profeaugustobriga@gmail>
2024-01-10 09:23:44 +0100

    Empleados_Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { Empleado } from './empleado';
+import { HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'listadoPersonal';
+  columnas: string[] = ['id', 'nombre', 'direccion', 'cargo' , 'edad' , 'imagen','eliminar','modificar'];
+  datos: Empleado[] =[];
+  urlString:string="http://moralo.atwebpages.com/ajaxListar/getTodoPersonal.php"
+  ds=new MatTableDataSource<Empleado>();  constructor( private httpCliente: HttpClient){
+
+   }
+  ngOnInit(): void {
+    this.httpCliente.get<Empleado[]>(this.urlString).subscribe((res)=>{  this.ds.data = res; this.datos=res});  }
 }
---
+++ src/app/empleado.ts
@@ -0,0 +1,3 @@
+export class Empleado {
+  id:number=0;  nombre:string='';   direccion:string='';  cargo:string='';  edad:number=0;  imagen:string='';
+}
